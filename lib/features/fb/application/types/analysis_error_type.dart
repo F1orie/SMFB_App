deb53d8197bd65faec17d49b2684c4e870ab4e35
yamanakario <rio0126rio0126@gmail.com>
2026-06-03 02:58:33 +0900

    chore: 未使用の旧FBファイルとtest.dartを削除
    削除対象（いずれもどこからもimportされていないことを確認済み）:
    - fb/application/state/analysis_state.dart
    - fb/application/types/ (3ファイル)
    - fb/application/usecases/analyze_chat_usecase.dart
    - fb/infrastructure/api/api_client.dart
    - fb/infrastructure/api/rag_repository.dart
    - fb/infrastructure/api/sleep_mock_data.dart
    - fb/presentation/router/fb_router.dart
    - common/test.dart

--- lib/features/fb/application/types/analysis_error_type.dart
+++
@@ -1,7 +0,0 @@
-// lib/features/fb/application/types/analysis_error_type.dart
-enum AnalysisErrorType {
-  network, // ネットが繋がっていない
-  timeout, // Pythonが重くて時間切れ
-  invalidKey, // APIキーがおかしい
-  unknown, // その他
-}
